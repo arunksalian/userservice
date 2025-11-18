3b3049db0b687800424705dc87141daa20082e3d
arunamg2025 <arun@assuremediagroup.com>
2025-11-18 10:52:57 +0530

    Fix test configuration to work without MongoDB connection

--- src/test/java/com/citpl/userservice/userservice/UserserviceApplicationTests.java
+++ src/test/java/com/citpl/userservice/userservice/UserserviceApplicationTests.java
@@ -1,10 +1,20 @@
 package com.citpl.userservice.userservice;
 
+import com.citpl.userservice.userservice.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.data.mongodb.uri=mongodb://localhost:27017/test",
+	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration"
+})
 class UserserviceApplicationTests {
+
+	@MockBean
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
